2b42e82f5d9f71a8f95fc35fdf0d80f686d08456
Zain-Ashfaq <zashfaq00@gmail.com>
2022-10-28 19:23:52 +0100

    mini change to update github page io

--- punk-app/src/components/SearchBar/SearchBar.jsx
+++ punk-app/src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,7 @@
 import "./SearchBar.scss";
 
 const SearchBar = ({ placeholder, onInput, value }) => {
+  // console.log("");
   return (
     <div className="main__searchbox">
       <input placeholder={placeholder} onInput={onInput} value={value}></input>
