f3870198c17f5880951647929fa085b746a001ee
Zain-Ashfaq <zashfaq00@gmail.com>
2022-10-28 19:15:23 +0100

    Click on card to show food pairings. Need css work

--- punk-app/src/components/ShowInfoCard/ShowInfoCard.jsx
+++ punk-app/src/components/ShowInfoCard/ShowInfoCard.jsx
@@ -1,10 +1,15 @@
+import "./ShowCardInfo.scss";
+
 const ShowInfoCard = ({ info }) => {
-  console.log("this is beer info in ShowInfoCard ");
+  console.log("this is beer info in ShowInfoCard ", info);
 
   return (
-    <>
-      <p>test</p>
-    </>
+    <div className="beercard_info">
+      <h2>Food Pairings</h2>
+      <p>{info[0]}</p>
+      <p>{info[1]}</p>
+      <p>{info[2]}</p>
+    </div>
   );
 };
 
--- punk-app/src/containers/BeerContainer/BeerContainer.jsx
+++ punk-app/src/containers/BeerContainer/BeerContainer.jsx
@@ -9,35 +9,51 @@ const BeerContainer = ({ beerData }) => {
 
   console.log(beerObj);
 
-  return (
-    <>
-      {beerData.map((beer) => {
-        // WHEN CLICKED, BEER INFO SHOULD APPEAR ON SCREEN
-        const handleCardClick = () => {
-          setBeerObj(beer.food_pairing);
+  if (!beerCardClicked) {
+    return (
+      <>
+        {beerData.map((beer) => {
+          // WHEN CLICKED, BEER INFO SHOULD APPEAR ON SCREEN
+          const handleCardClick = () => {
+            setBeerObj(beer.food_pairing);
 
-          // console.log(`Beer id ${beer.id} and ${beer.name} has been clicked`);
-          // console.log(`Beer id ${beer.id} and ${beer.name} has been clicked`);
-        };
+            setBeerCardClicked(!beerCardClicked);
 
-        // console.log(beerObj);
+            // console.log(`Beer id ${beer.id} and ${beer.name} has been clicked`);
+            // console.log(`Beer id ${beer.id} and ${beer.name} has been clicked`);
+          };
 
-        return (
-          <div className="beercard_container" onClick={handleCardClick}>
-            <Card
-              id={beer.id}
-              beerName={beer.name}
-              image_url={beer.image_url}
-              tagline={beer.tagline}
-              firstBrewed={beer.first_brewed}
-              abvNumber={beer.abv}
-              ph={beer.ph}
-            />
-          </div>
-        );
-      })}
-    </>
-  );
+          // console.log(beerObj);
+
+          return (
+            <div className="beercard_container" onClick={handleCardClick}>
+              <Card
+                id={beer.id}
+                beerName={beer.name}
+                image_url={beer.image_url}
+                tagline={beer.tagline}
+                firstBrewed={beer.first_brewed}
+                abvNumber={beer.abv}
+                ph={beer.ph}
+              />
+            </div>
+          );
+        })}
+      </>
+    );
+  } else {
+    const handleCardClick = () => {
+      setBeerCardClicked(!beerCardClicked);
+
+      // console.log(`Beer id ${beer.id} and ${beer.name} has been clicked`);
+      // console.log(`Beer id ${beer.id} and ${beer.name} has been clicked`);
+    };
+    return (
+      <div onClick={handleCardClick}>
+        <ShowInfoCard info={beerObj} />
+      </div>
+    );
+  }
 };
 
 export default BeerContainer;
